259df82e1b0355a1681ee4dddf89d8b9404219d1
JUAN CARLOS LOPEZ GARCIA <juanca.lopez@extern.isban.com>
2016-11-02 12:06:24 +0100

    Generación de Controller

---
+++ SpringHibernate/src/main/java/com/vector/controller/WadlController.java
@@ -0,0 +1,54 @@
+package com.vector.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import net.java.dev.wadl._2009._02.Application;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import com.autentia.web.rest.wadl.builder.ApplicationBuilder;
+import com.autentia.web.rest.wadl.builder.impl.springframework.SpringWadlBuilderFactory;
+import com.autentia.xml.schema.SchemaBuilder;
+
+@Controller
+@RequestMapping(value="/rest", produces={"application/xml"})
+public class WadlController {
+
+	private final ApplicationBuilder applicationBuilder;
+    private final SchemaBuilder schemaBuilder;
+    
+    @Autowired
+    public WadlController(RequestMappingHandlerMapping handlerMapping){
+    	final SpringWadlBuilderFactory wadlBuilderFactory = new SpringWadlBuilderFactory(handlerMapping);
+    	applicationBuilder = wadlBuilderFactory.getApplicationBuilder();
+    	schemaBuilder = wadlBuilderFactory.getSchemaBuilder();
+    }
+    
+    @RequestMapping(value="/wadl", method=RequestMethod.GET)
+    @ResponseBody
+    public Application genrateWADL(HttpServletRequest request){
+    	return applicationBuilder.build(request);
+    }
+    
+    @RequestMapping(value="schema/{classTypeName}", method=RequestMethod.GET)
+    @ResponseBody
+    public String generateSchema(@PathVariable String classTypeName){
+    	return schemaBuilder.buildFor(classTypeName);
+    }
+    
+    @ResponseStatus(value=HttpStatus.NOT_FOUND)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public void handlerException(){
+    	
+    }
+    
+}
